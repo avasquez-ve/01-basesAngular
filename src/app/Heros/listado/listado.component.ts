696db35095ceaf924698d0400c7c5dc1f7a242e3
Arnell Vasquez <24895868+avasquez-ve@users.noreply.github.com>
2021-10-27 23:18:35 -0300

    Creacion de un modulo de forma automatica

---
+++ src/app/Heros/listado/listado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+//ng g c heros/listado --> Genera componente automatico
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AccumulatorComponent } from './Acumulator/accumulator.component';
 import { HeroComponent } from './Heros/Hero/hero.component';
+import { ListadoComponent } from './heros/listado/listado.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AccumulatorComponent,
-    HeroComponent
+    HeroComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule
