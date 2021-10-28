b8ccf3a9d6d8c9e84bb1283caf8d9b887daae92d
Arnell Vasquez <24895868+avasquez-ve@users.noreply.github.com>
2021-10-27 23:11:04 -0300

    Uso de concepto one way data binding(siempre recomendado)

--- src/app/Heros/Hero/hero.component.ts
+++ src/app/Heros/Hero/hero.component.ts
@@ -5,5 +5,23 @@ import { Component } from "@angular/core";
     templateUrl: "hero.component.html"
 })
 export class HeroComponent{
+    //props
+    public nombre: string = "Iron man";
+    public edad: number = 45;
 
+    //getters-setters
+    get nombreCapitalizado(): string {
+        return this.nombre.toUpperCase();
+    }
+
+    //Methods
+    combinarNombre(): string{
+        return `${this.nombre} - ${this.edad}`
+    }
+    cambiarNombre():void {
+        this.nombre = "Spider man";
+    }
+    cambiarEdad():void {
+        this.edad = 16;
+    }
 }
