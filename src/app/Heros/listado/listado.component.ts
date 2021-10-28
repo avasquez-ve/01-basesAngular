76c4be741f6071898bb2d0bfcfd7df7967e3da41
Arnell Vasquez <24895868+avasquez-ve@users.noreply.github.com>
2021-10-28 12:42:55 -0300

    Uso de ciclo ngFor en el document

--- src/app/Heros/listado/listado.component.ts
+++ src/app/Heros/listado/listado.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './listado.component.html'
 })
 export class ListadoComponent {
+  //props
+  public heros: string[] = ["Iron Man", "Spider Man", "Thor", "Dr. Strange", "Black Widow", "Hawkeye"];
+  public deletedHeros: string[] = [];
 
+  //methods
+  deleteRandomHero():void {
+    let arrLength:number = this.heros.length + 1;
+    let itemToRemove:number = Math.floor(Math.random() *  this.heros.length);
+    let deletedHeros:string[] = this.heros.splice(itemToRemove, 1);
+
+    if (deletedHeros[0] != undefined) {
+      this.deletedHeros.push(deletedHeros[0]);
+    }
+  }
 }
