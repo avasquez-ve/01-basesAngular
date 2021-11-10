1111bb155ec4d0feed178f7fe68d2e44490f8913
Arnell Vasquez <24895868+avasquez-ve@users.noreply.github.com>
2021-11-09 22:32:36 -0300

    Ng Model y two data binding

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Character {
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-mainPage',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  templateUrl: './main-page.component.html'
 })
-export class MainPageComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+export class MainPageComponent {
+  
+  //global var
+  character: Character = {
+    name: 'Trunks',
+    power: 18000
   }
 
+  //Methods
+  addDataToCharacter(): void{
+    console.log(this.character);
+  }
 }
